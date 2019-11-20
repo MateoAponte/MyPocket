4cd521428f232e1680c96a8363b9a33609db3ca0
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-19 22:37:25 -0500

    Se realiza la configuración Basica del Servidor con Router

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config';
+import webpack from 'webpack';
+
+// Settings
+app.set('port', process.env.PORT || 8081);
+// Middlewares
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({extended: false}))
+    app.use(webpackDevMiddleware(webpack(webpackConfig)));
+    
+    // Router
+    app.use(require('./router/router.js'));
+    
+    // Static
+    app.use(express.static(path.join(__dirname, './dist')));
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+      extended: true
+    })); 
+
+
+    app.listen(app.get('port'), () => {
+        console.log(`server on port ${app.get('port')}`);
+      });
+      
