fa67e7d59aed2cfdb8f39620d79a79b5d618633e
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-26 20:11:59 -0500

    Vista primitiva del Registro hecha

---
+++ src/views/userAdmin/Registrer.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="m-general-container">
+        <div class="m-head-container form-control">
+            <div class="m-head-container-card">
+                <div class="m-container-item--row--center">
+                    <span class="m-title-big">
+                        REGISTRO
+                    </span>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Nombre:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Apellido:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Presupuesto:
+                    </label>
+                    <input type="text" class="custom-form">
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Contraseña:
+                    </label>
+                    <input type="password" class="custom-form">
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Confirma Contraseña:
+                    </label>
+                    <input type="password" class="custom-form">
+                </div>
+                <div class="m-container-item--row">
+                    <button class="m-button m-button-turquese m-button-long">Ingresar</button>
+                </div>
+                <div class="m-container-item--column">
+                    <span class="m-mini-paragraph">
+                        Si ya estas registrado clickea <router-link to="/">Aquí</router-link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
