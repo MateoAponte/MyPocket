225b82254eae30a35eae74d34c2f0b95cc0a9458
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-26 20:12:24 -0500

    Vista primitiva de la Confirmación hecha

---
+++ src/views/userAdmin/Confirmation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="m-general-container">
+        <div class="m-head-container form-control">
+            <div class="m-head-container-card">
+                <div class="m-container-item--row--center">
+                    <span class="m-title-big">
+                        De click en el siguiente link para continuar con la Configuración de su perfil  <br/>
+                        <router-link to="/configuration">Aquí</router-link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
