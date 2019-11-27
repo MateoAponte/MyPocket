8f0efd1fb73921ca7082ae4975a9e6d5ffc9288c
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-26 20:12:47 -0500

    Vista primitiva de la Configuración hecha

---
+++ src/views/config/Configuration.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="m-general-container">
+        <div class="m-head-container form-control">
+            <div class="m-head-container-card">
+                <div class="m-container-item--row--center">
+                    <span class="m-title-big">
+                        CONFIGURACIÓN
+                    </span>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Eres:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                    <div class="m-container-item--column">
+                        <input type="text" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Nombre:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Apellido:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column--mini">
+                        <label class="m-label">
+                            Interes:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Presupuesto:
+                        </label>
+                        <input type="text" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Contraseña:
+                        </label>
+                        <input type="password" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--row">
+                    <div class="m-container-item--column">
+                        <label class="m-label">
+                            Confirmación:
+                        </label>
+                        <input type="password" class="custom-form">
+                    </div>
+                </div>
+                <div class="m-container-item--row">
+                    <button class="m-button m-button-turquese m-button-long">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
