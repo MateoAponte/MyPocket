c4e564bcdd8a8eebb30c12767971fb593e5be653
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-26 20:12:05 -0500

    Vista primitiva del Login hecha

---
+++ src/views/userAdmin/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="m-general-container">
+        <div class="m-head-container form-control">
+            <div class="m-head-container-card">
+                <div class="m-container-item--row--center">
+                    <span class="m-title-big">
+                        LOGIN
+                    </span>
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Nombre:
+                    </label>
+                    <input type="text" class="custom-form">
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Apellido: 
+                    </label>
+                    <input type="text" class="custom-form">
+                </div>
+                <div class="m-container-item--column">
+                    <label class="m-label">
+                        Contraseña:
+                    </label>
+                    <input type="password" class="custom-form">
+                </div>
+                <div class="m-container-item--row">
+                    <button class="m-button m-button-orange m-button-long">Ingresar</button>
+                </div>
+                <div class="m-container-item--column">
+                    <span class="m-mini-paragraph">
+                        Si no estas registrado Clickea <router-link to="/">Aquí</router-link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
