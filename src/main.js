e960f66c9da18888c23831be65a195f6d770c0d6
MateoAponte <52506514+MateoAponte@users.noreply.github.com>
2019-11-28 00:03:57 -0500

    Se instalo la dependencia de Font-Awesome para usar sus iconos como componentes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from '@Views/App';
 import router from '@Router/router';
 import store from '@Store/store';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMapPin, faFlagCheckered, faFlag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import moment from 'moment';
 import _ from 'lodash';
 import numeral from 'numeral';
@@ -12,6 +16,9 @@ Vue.set(Vue.prototype, "moment", moment);
 Vue.set(Vue.prototype, "_", _);
 Vue.set(Vue.prototype, "numeral", numeral);
 Vue.set(Vue.prototype, "is", is);
+
+library.add(faMapPin, faFlagCheckered, faFlag);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 window.eventBus = new Vue();
 Vue.config.productionTip = false;
