c0a741a66237e018e122cdb347e0c6f0c2f17615
Mateo Aponte <mateo2aponte@hotmail.com>
2020-01-20 23:39:53 -0500

    Arreglo de Peticiones POST

--- server/router/router.js
+++ server/router/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const sync = require('sync');
 const router = express.Router();
 const fs = require('fs');
 const bodyParser = require('body-parser');
@@ -10,107 +10,73 @@ const date = new Date();
 const month = date.getMonth();
 const year = date.getFullYear();  
 
+function convertMonth(value){
+    value = value + 1;
+    return value < 10 ? `0${value}` : value
+}
+
 router.get('/', (req, res) => {
     res.redirect('index.html')
 })
 
 router.post('/registrer', jsonParser, (req, res) => {  
-    let infoUser = req.body.configUser;
-    let fullname = infoUser.name + infoUser.lastname;
-    dataUser.configUser = infoUser;
-    if(fs.existsSync(`./server/api/${fullname}`)){
-        dataUser.configUser.status = 'Loged';
+    let infoUser = req.body.userData;
+    dataUser.userData = infoUser;
+    dataUser.configData = {};
+    let user = infoUser.user;
+    if(fs.existsSync(`./server/api/${user}`)){
+        dataUser.userData.status = 'Loged';
         res.send(dataUser);
     } else {
-        dataUser.configUser.status = 'toLog';
-        fs.mkdirSync(`./server/api/${fullname}`);
-        fs.writeFileSync(`./server/api/${fullname}/data${month}-${year}.json`, JSON.stringify(dataUser), 'utf-8');
+        dataUser.userData.status = 'toLog';
+        fs.mkdirSync(`./server/api/${user}`);
+        fs.writeFileSync(`./server/api/${user}/config.json`, JSON.stringify(dataUser), 'utf-8');
+        fs.writeFileSync(`./server/api/${user}/data${convertMonth(month)}-${year}.json`, JSON.stringify( {itemsData: []} ), 'utf-8');
     }
 });
 
 router.post('/login', jsonParser, (req, res) => {
-    let infoUser = req.body.configUser;
-    let fullname = infoUser.name + infoUser.lastname;
-    dataUser.configUser = infoUser;
-    if(fs.existsSync(`./server/api/${fullname}`)){
-        fs.readdir(`./server/api/${fullname}`, function(err, files){
+    let infoUser = req.body.userData;
+    dataUser.userData = infoUser;
+    let user = infoUser.user;
+    if(fs.existsSync(`./server/api/${user}`)){
+        fs.readFile(`./server/api/${user}/config.json`, 'utf-8', function(err, data){
+            dataUser = JSON.parse(data);
+        });
+        fs.readdir(`./server/api/${user}`, function(err, files){
             files.forEach( x => {
-                if(fs.existsSync(`./server/api/${fullname}/data${month}-${year}.json`)){
-                    fs.readFile(`./server/api/${fullname}/data${month}-${year}.json`, 'utf-8', function(err, data){
-                        dataUser = JSON.parse(data);
-                        dataUser.configUser.status = 'Loged';
-                        fs.writeFileSync(`./server/api/${fullname}/data${month}-${year}.json`,JSON.stringify(dataUser), 'utf-8');
-                        res.send(dataUser.configUser);
+                if(fs.existsSync(`./server/api/${user}/data${convertMonth(month)}-${year}.json`)){
+                    fs.readFile(`./server/api/${user}/data${convertMonth(month)}-${year}.json`, 'utf-8', function(err, data){
+                        dataUser = Object.assign(dataUser, JSON.parse(data));
+                        res.send(dataUser);
                     });
                 } else {
-                    fs.readFile(`./server/api/${fullname}/data${month}-${year}.json`, 'utf-8', function(err, data){
-                        dataUser = JSON.stringify(data);
-                        dataUser.configUser.status = 'Loged';
-                        fs.writeFileSync(`./server/api/${fullname}/data${month - 1}-${year}.json`, JSON.stringify(dataUser), 'utf-8');
-                        res.send(dataUser.configUser);
-                    });
+                    sync(function(){
+                        fs.writeFileSync(`./server/api/${user}/data${convertMonth(month)}-${year}.json`, JSON.stringify( {itemsData: []} ), 'utf-8')
+                        fs.readFile(`./server/api/${user}/data${convertMonth(month)}-${year}.json`, 'utf-8', function(err, data){
+                            dataUser = Object.assign(dataUser, JSON.parse(data));
+                            res.send(dataUser);
+                        })
+                    })
                 }
             } )
         })
     } else {
-        dataUser.configUser.status = 'notExist';
+        dataUser.userData.status = 'notExist';
         res.send(dataUser);
     }
 })
 
-router.post('/confirmation', (req, res) => {
-    let date = new Date();
-    let month = date.getMonth();
-    let year = date.getFullYear();
+router.post('/save', jsonParser, (req, res) => {
+    dataUser.itemsData = req.body.itemsData;
+    let user = infoUser.user;
 
-    req.body = {userInitialInfo: req.body}
-    let reqUserInfo = req.body.userInitialInfo
-    let fullname = reqUserInfo.name + reqUserInfo.lastname;
-
-    dataUser.push(req.body);
-    const jsonData = JSON.stringify(dataUser);
-    fs.readdir(`./src/api/`, function(err, files){
-        if(files.includes(fullname)){
-            files.forEach(file => {
-                if(file == fullname){
-                    fs.readdir(`./src/api/${fullname}`, function(err, subFiles){
-                        if(subFiles.includes(`data${month}-${year}.json`)){
-                            res.redirect('/#/login');
-                        } else {
-                            fs.writeFileSync(`./src/api/${fullname}/data${month}-${year}.json`, jsonData, 'utf-8');
-                            res.redirect('/#/confirmation');
-                        }
-                    })
-                }
-            });
-        } else {
-            fs.mkdirSync(`./src/api/${fullname}`);
-            fs.writeFileSync(`./src/api/${fullname}/data${month}-${year}.json`, jsonData, 'utf-8');
-            res.redirect('/#/confirmation');
-        }
-    })
+    fs.writeFileSync(`./server/api/${user}/data${convertMonth(month)}-${year}.json`, JSON.stringify(dataUser), 'utf-8');
+    res.send("your save is Successfull");
 })
 
-router.post('/save', (req, res) => {
-    let {data} = req.body;
-    let parseData = JSON.parse(data);
-    let name = parseData[0].userInitialInfo.name;
-    let lastname = parseData[0].userInitialInfo.lastname;
-    let fullname = name + lastname;
-
-    let date = new Date();
-    let month = date.getMonth();
-    let year = date.getFullYear();
-
-    fs.writeFileSync(`./src/api/${fullname}/data${month}-${year}.json`, data, 'utf-8');
-    res.redirect(`/#/Financias?name=${name}&lastname=${lastname}`);
-})
-
-router.get('/dataUser', (req, res) => {
-    let query = req.query;
-    fs.readFile(`./src/api/${query.dir}/data${query.month}-${query.year}.json`, 'utf8', (err, data) => {
-        res.send(data);
-    })
+router.get('/configuration', (req, res) => {
+    fs.readdir(`./server/api/${user}`, function(err, files){});
 })
 
 module.exports = router;
